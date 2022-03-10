ce4e7f20e5cfa51adfd70c537480955aff68ecaa
mind28 <96925869+mind28@users.noreply.github.com>
2022-03-10 15:17:00 -0500

    debugging

--- index.js
+++ index.js
@@ -23,8 +23,12 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
-mongoose.connect('mongodb://localhost:27017/test', { 
-    useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/test', { 
+//     useNewUrlParser: true, useUnifiedTopology: true });
+
+// mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 
 app.use(morgan('common'));
 app.use(express.static('public'));
