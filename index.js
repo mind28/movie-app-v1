6bce5076d8234d05241953698e01baeff64815b6
mind28 <96925869+mind28@users.noreply.github.com>
2022-03-10 15:24:53 -0500

    debugging

--- index.js
+++ index.js
@@ -26,9 +26,10 @@ require('./passport');
 // mongoose.connect('mongodb://localhost:27017/test', { 
 //     useNewUrlParser: true, useUnifiedTopology: true });
 
-// mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
+// mongoose.connect('mongodb+srv://mind28:Black-Mamba24@movie-app.xixo1.mongodb.net/movie-app?retryWrites=true&w=majority', { 
+//     useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(morgan('common'));
 app.use(express.static('public'));
